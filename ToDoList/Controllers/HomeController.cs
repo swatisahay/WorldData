fbd02cf7820d4919b8e1468d32914d17623ea0e7
Brian Palowski, Colin Bates and Eddie Harris <colin@>
2018-07-03 10:26:08 -0700

    add csharp example files with multiple controllers

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+         [HttpGet("/")]
+        // public ActionResult Index()
+        // {
+        //     return View();
+        // }
+
+        public ActionResult Index()
+        {
+          return new EmptyResult();
+        }
+
+
+    }
+}
